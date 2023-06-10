8d0ad135de2cfc3064e9026e741ddebfdb5e2da5
shreyash dhamane <shreyashb.dhamane0@gmail.com>
2023-06-10 23:08:40 +0530

    .

--- controllers/orders.controllers.js
+++ controllers/orders.controllers.js
@@ -127,4 +127,208 @@ const add = async (req, res) => {
   }
 };
 
-module.exports = { add };
+const getTodaysTotalOrders = async (req, res) => {
+  try {
+    var { date } = req.query;
+    var month = date.split("-")[1];
+    //add 0 to the month if it is less than 10
+    if (parseInt(month) < 10) {
+      month = "0" + month;
+    }
+    var day = date.split("-")[2];
+    //add 0 to the day if it is less than 10
+    if (parseInt(day) < 10) {
+      day = "0" + day;
+    }
+    var newDate = date.split("-")[0] + "-" + month + "-" + day;
+    const ordersForDay = await Order.find({ date: newDate }, { order: 1 });
+    //go thorught the orders and add the total number of orders for each item
+    var totalOrdersOfEachItem = {};
+    var totalAmount = 0;
+    for (var i = 0; i < ordersForDay.length; i++) {
+      const order = ordersForDay[i].order;
+      totalAmount += order.totalAmount;
+      if (order.hasOwnProperty("curryRiceBowlOrders")) {
+        const curryRiceBowlOrders = order.curryRiceBowlOrders;
+        // console.log(curryRiceBowlOrders);
+        for (var j = 0; j < curryRiceBowlOrders.length; j++) {
+          const riceBowlOrder = curryRiceBowlOrders[j];
+          if (riceBowlOrder.name == "butter chicken") {
+          }
+          if (riceBowlOrder.hasOwnProperty("totalLargeBowlQuantity")) {
+            var largeBowlName = "large " + riceBowlOrder.name;
+            //if the item is already in the object, add the quantity to the existing quantity
+            if (totalOrdersOfEachItem.hasOwnProperty(largeBowlName)) {
+              totalOrdersOfEachItem[largeBowlName] =
+                totalOrdersOfEachItem[largeBowlName] +
+                riceBowlOrder.totalLargeBowlQuantity;
+            } else {
+              totalOrdersOfEachItem[largeBowlName] =
+                riceBowlOrder.totalLargeBowlQuantity;
+            }
+          }
+          if (riceBowlOrder.hasOwnProperty("totalSmallBowlQuantity")) {
+            var smallBowlName = "small " + riceBowlOrder.name;
+            //if the item is already in the object, add the quantity to the existing quantity
+            if (totalOrdersOfEachItem.hasOwnProperty(smallBowlName)) {
+              totalOrdersOfEachItem[smallBowlName] =
+                totalOrdersOfEachItem[smallBowlName] +
+                riceBowlOrder.totalSmallBowlQuantity;
+            } else {
+              totalOrdersOfEachItem[smallBowlName] =
+                riceBowlOrder.totalSmallBowlQuantity;
+            }
+          }
+        }
+      }
+      if (order.hasOwnProperty("indianPlateOrders")) {
+        const indianPlateOrders = order.indianPlateOrders;
+        for (var j = 0; j < indianPlateOrders.length; j++) {
+          const indianPlateOrder = indianPlateOrders[j];
+          const name = indianPlateOrder.name;
+          const quantity = indianPlateOrder.quantity;
+          var newName = name + " " + "indian plate";
+          if (totalOrdersOfEachItem.hasOwnProperty(newName)) {
+            totalOrdersOfEachItem[newName] =
+              totalOrdersOfEachItem[newName] + quantity;
+          } else {
+            totalOrdersOfEachItem[newName] = quantity;
+          }
+        }
+      }
+      if (order.hasOwnProperty("sidesOrders")) {
+        const sidesOrders = order.sidesOrders;
+        for (var j = 0; j < sidesOrders.length; j++) {
+          const sideOrder = sidesOrders[j];
+          const name = sideOrder.name;
+          const quantity = sideOrder.quantity;
+          if (quantity == 0) {
+            continue;
+          }
+          var newName = name + " " + "side";
+          if (totalOrdersOfEachItem.hasOwnProperty(name)) {
+            totalOrdersOfEachItem[name] =
+              totalOrdersOfEachItem[name] + quantity;
+          } else {
+            totalOrdersOfEachItem[name] = quantity;
+          }
+        }
+      }
+    }
+    var totalOrdersForEachItemNameToQuantityObjectArray = [];
+    for (var key in totalOrdersOfEachItem) {
+      totalOrdersForEachItemNameToQuantityObjectArray.push({
+        name: key,
+        quantity: totalOrdersOfEachItem[key],
+      });
+    }
+    //sort the array in descending order, the item with the highest quantity will be at the top
+    totalOrdersForEachItemNameToQuantityObjectArray.sort(function (a, b) {
+      return b.quantity - a.quantity;
+    });
+
+    res
+      .status(200)
+      .json({ totalOrdersForEachItemNameToQuantityObjectArray, totalAmount });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getTodaysTimedOrders = async (req, res) => {
+  try {
+    var { date } = req.query;
+    var month = date.split("-")[1];
+    //add 0 to the month if it is less than 10
+    if (parseInt(month) < 10) {
+      month = "0" + month;
+    }
+    var day = date.split("-")[2];
+    //add 0 to the day if it is less than 10
+    if (parseInt(day) < 10) {
+      day = "0" + day;
+    }
+    var newDate = date.split("-")[0] + "-" + month + "-" + day;
+    const ordersForDay = await Order.find(
+      { date: newDate },
+      { order: 1, time: 1 }
+    );
+
+    // we have to find number of orders and amount in given time slots,
+    // time slots are 11-12, 12- 1, 1-2, 2-3, 3-4, 4-5, 5-6, 6-7, 7-8, 8-9, 9-10
+    // we will create an object with keys as time slots and values as number of orders and amount
+    var timeSlots = {
+      "11-12": { numberOfOrders: 0, amount: 0 },
+      "12-1": { numberOfOrders: 0, amount: 0 },
+      "1-2": { numberOfOrders: 0, amount: 0 },
+      "2-3": { numberOfOrders: 0, amount: 0 },
+      "3-4": { numberOfOrders: 0, amount: 0 },
+      "4-5": { numberOfOrders: 0, amount: 0 },
+      "5-6": { numberOfOrders: 0, amount: 0 },
+      "6-7": { numberOfOrders: 0, amount: 0 },
+      "7-8": { numberOfOrders: 0, amount: 0 },
+      "8-9": { numberOfOrders: 0, amount: 0 },
+      "9-10": { numberOfOrders: 0, amount: 0 },
+    };
+    var totalAmount = 0;
+    for (var i = 0; i < ordersForDay.length; i++) {
+      const order = ordersForDay[i].order;
+      var time = ordersForDay[i].time;
+      const amount = order.totalAmount;
+      time = parseInt(time.split(":")[0]);
+
+      totalAmount = totalAmount + amount;
+
+      if (time == 11) {
+        timeSlots["11-12"].numberOfOrders =
+          timeSlots["11-12"].numberOfOrders + 1;
+        timeSlots["11-12"].amount = timeSlots["11-12"].amount + amount;
+      } else if (time == 12) {
+        timeSlots["12-1"].numberOfOrders = timeSlots["12-1"].numberOfOrders + 1;
+        timeSlots["12-1"].amount = timeSlots["12-1"].amount + amount;
+      } else if (time == 1) {
+        timeSlots["1-2"].numberOfOrders = timeSlots["1-2"].numberOfOrders + 1;
+        timeSlots["1-2"].amount = timeSlots["1-2"].amount + amount;
+      } else if (time == 2) {
+        timeSlots["2-3"].numberOfOrders = timeSlots["2-3"].numberOfOrders + 1;
+        timeSlots["2-3"].amount = timeSlots["2-3"].amount + amount;
+      } else if (time == 3) {
+        timeSlots["3-4"].numberOfOrders = timeSlots["3-4"].numberOfOrders + 1;
+        timeSlots["3-4"].amount = timeSlots["3-4"].amount + amount;
+      } else if (time == 4) {
+        timeSlots["4-5"].numberOfOrders = timeSlots["4-5"].numberOfOrders + 1;
+        timeSlots["4-5"].amount = timeSlots["4-5"].amount + amount;
+      } else if (time == 5) {
+        timeSlots["5-6"].numberOfOrders = timeSlots["5-6"].numberOfOrders + 1;
+        timeSlots["5-6"].amount = timeSlots["5-6"].amount + amount;
+      } else if (time == 6) {
+        timeSlots["6-7"].numberOfOrders = timeSlots["6-7"].numberOfOrders + 1;
+        timeSlots["6-7"].amount = timeSlots["6-7"].amount + amount;
+      } else if (time == 7) {
+        timeSlots["7-8"].numberOfOrders = timeSlots["7-8"].numberOfOrders + 1;
+        timeSlots["7-8"].amount = timeSlots["7-8"].amount + amount;
+      } else if (time == 8) {
+        timeSlots["8-9"].numberOfOrders = timeSlots["8-9"].numberOfOrders + 1;
+        timeSlots["8-9"].amount = timeSlots["8-9"].amount + amount;
+      } else if (time == 9) {
+        timeSlots["9-10"].numberOfOrders = timeSlots["9-10"].numberOfOrders + 1;
+        timeSlots["9-10"].amount = timeSlots["9-10"].amount + amount;
+      }
+    }
+
+    //convert the object to an array of objects
+    var timeSlotsArray = [];
+    for (var key in timeSlots) {
+      timeSlotsArray.push({
+        timeSlot: key,
+        numberOfOrders: timeSlots[key].numberOfOrders,
+        amount: timeSlots[key].amount,
+      });
+    }
+    res.status(200).json({ timeSlotsArray, totalAmount });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { add, getTodaysTotalOrders, getTodaysTimedOrders };
--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -1,7 +1,14 @@
-const { add } = require("../controllers/orders.controllers");
+const {
+  add,
+  getTodaysTotalOrders,
+  getTodaysTimedOrders,
+} = require("../controllers/orders.controllers");
 
 const router = require("express").Router();
 
 router.post("/", add);
+// router.get("/daily_detail", getTodaysTotalOrders);
+router.get("/daily_total", getTodaysTotalOrders);
+router.get("/daily_timed_sales", getTodaysTimedOrders);
 
 module.exports = router;
